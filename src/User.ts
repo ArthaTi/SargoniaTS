998f47bfaee6ae20c511ede7ef5704ac1f220e8b
SirErico <eryklol88888@gmail.com>
2019-08-23 21:38:00 +0200

    User characters link

--- src/User.ts
+++ src/User.ts
@@ -1,9 +1,12 @@
+import Character from "./Character";
+
 export default class User {
 
     static lastID: number = 0;
 
     id!: number;
-
+    characters?: Character[]
+    
     constructor(public name: string = "", public passwordHash: string = "") {
 
         this.id = ++User.lastID;
