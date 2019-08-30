e59ee1958e8b9ad0ab8824bd23b186c1ccc8074a
Soaku <dominik@drozak.net>
2019-08-30 17:50:52 +0200

    Add "lastUsed" property to character

--- src/Character.ts
+++ src/Character.ts
@@ -4,10 +4,18 @@ export default class Character {
 
     static lastID: number = 0;
 
+    /**
+     * ID of the character.
+     */
     id!: number;
+
     event?: Event;
     fight?: Fight;
-    
+
+    /**
+     * The last time the character was in use.
+     */
+    lastUse: number = Date.now();
 
     constructor(public name: string = "") {
 
---
+++ src/actions/characters.ts
@@ -0,0 +1,7 @@
+import { actions } from "..";
+
+actions["characters"] = context => {
+
+    
+
+};
